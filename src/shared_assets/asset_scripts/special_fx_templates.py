a2377cd9093d7d0d2e75add898c1711048882269
Daniel Konar <konarsonarsmonar@gmail.com>
2024-09-16 19:19:58 +0200

    Add discover magic effect

--- src/shared_assets/asset_scripts/special_fx_templates.py
+++ src/shared_assets/asset_scripts/special_fx_templates.py
@@ -5,6 +5,9 @@ def effect(template: str, to_args: Callable[[Dict], Dict]) -> Callable[[Dict], s
 	return lambda args: template.format(**to_args(args))
 
 
+def no_args(_: Dict) -> Dict:
+	return {}
+
 phase_template = """static func special_effect(_castercrea, _spell, _power, _main_targeted_tile, _effected_tiles, _effected_creas, _add_terrain) :
 	var newtpos : Vector2 = _main_targeted_tile
 	_castercrea.combat_button.position  = Utils.GRID_SIZE * newtpos
@@ -27,6 +30,33 @@ def phase_args(args: Dict) -> Dict:
 	print(result)
 	return result
 
+discover_magic_template = """static func special_effect(_castercrea, _spell, _power, _main_targeted_tile, _effected_tiles, _effected_creas, _add_terrain) -> bool :
+	var text : String = ''
+	for c : Creature in _effected_creas :
+		var c_magic_items : Array = []
+		for i : Dictionary in c.inventory :
+			if i['is_magical'] : c_magic_items.append(i['name'])
+		if c_magic_items.is_empty() :
+			text += c.name + ' carries no magic item.\\n'
+		else :
+			text += c.name + ' carries magic items :\\n'
+			for i : int in range(c_magic_items.size()) :
+				if i < c_magic_items.size() :
+					text += c_magic_items[i] +', '
+				else :
+					text += c_magic_items[i] +'\\n'
+	var textRect = UI.ow_hud.textRect
+	if StateMachine.is_combat_state() :
+		textRect.show()
+		UI.ow_hud.creatureRect.hide()
+	textRect.set_text(text, true)
+	await textRect.interruption_over
+	if StateMachine.is_combat_state() :
+		textRect.hide()
+		UI.ow_hud.creatureRect.show()
+	return true"""
+
 special_fx = {
 	56: effect(phase_template, phase_args),
+	63: effect(discover_magic_template, no_args),
 }
