d29454f78d13f93c74ed07b991189c66b08d4c8a
SamuelRabreau <samuel.rabreau@outlook.fr>
2025-05-11 15:35:55 +0200

    CoB Map tiles import, new tilesets
    imported all CoB maps tile data.
    Remade the Castle tileset.
    Made an Overlays tileset.

--- src/Campaigns/City of Bywater/on_campaign_start.gd
+++ src/Campaigns/City of Bywater/on_campaign_start.gd
@@ -10,7 +10,7 @@ static func before_loading_ressources() :
 
 static func after_loading_ressources() :
 	GameGlobal.time = 86401	#set starting dime  (day 1, 1s)
-	GameGlobal.currentmap_name = "test_map"	# set starting map
+	GameGlobal.currentmap_name = "map_1"	# set starting map
 	#GameGlobal.currentmap_name = "new_map_tutorial"
 	#GameGlobal.allow_character_swap_anywhere = true
 	set_boats_in_gameglobal()
@@ -18,11 +18,13 @@ static func after_loading_ressources() :
 
 #sets boat data in GameGlobal.map_boats_dict
 static func set_boats_in_gameglobal() :
-	GameGlobal.map_boats_dict = {"test_map" : {  "67,50" :  "Funny2" } }
+	pass
+	#GameGlobal.map_boats_dict = {"test_map" : {  "67,50" :  "Funny2" } }
 	
 static func set_minimaps_in_gameglobal() :
-	GameGlobal.minimaps = [
-		#"MinimapName, MapItRepresents, splashimagename, descroiption, topleftcoordinates, pixels/tile, owned
-		["City of Bywater NW", "test_map", "CityMiniMap.png","A map of the City of Bywater. The General Store is marked.", [5,5], 16, 1],
-		["Wilderness", "test_map", [20,20], "wilderness.png", "mostly forest and shit.", 1, 0]
-	]
+	pass
+	#GameGlobal.minimaps = [
+	#	#"MinimapName, MapItRepresents, splashimagename, descroiption, topleftcoordinates, pixels/tile, owned
+	#	["City of Bywater NW", "test_map", "CityMiniMap.png","A map of the City of Bywater. The General Store is marked.", [5,5], 16, 1],
+	#	["Wilderness", "test_map", [20,20], "wilderness.png", "mostly forest and shit.", 1, 0]
+	#]
