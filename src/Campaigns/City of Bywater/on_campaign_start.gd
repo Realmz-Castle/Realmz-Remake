5b3a0007b93af287ac47c61f8ad0689a74ec7e34
SamuelRabreau <samuel.rabreau@outlook.fr>
2025-05-12 14:56:45 +0200

    fixed CoB Battles json
    fixed CoB Battles json

--- src/Campaigns/City of Bywater/on_campaign_start.gd
+++ src/Campaigns/City of Bywater/on_campaign_start.gd
@@ -4,13 +4,13 @@ static func before_loading_ressources() :
 	print("City of bywater before_loading_ressources() ")
 	var owchar = NodeAccess.__Map().owcharacter
 	owchar .position = Vector2(3,3) * Utils.GRID_SIZE
-	owchar.tile_position_x = 3
-	owchar.tile_position_y = 3
+	owchar.tile_position_x = 2
+	owchar.tile_position_y = 1
 	pass
 
 static func after_loading_ressources() :
 	GameGlobal.time = 86401	#set starting dime  (day 1, 1s)
-	GameGlobal.currentmap_name = "map_1"	# set starting map
+	GameGlobal.currentmap_name = "map_0"	# set starting map
 	#GameGlobal.currentmap_name = "new_map_tutorial"
 	#GameGlobal.allow_character_swap_anywhere = true
 	set_boats_in_gameglobal()
